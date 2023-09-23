381348e395eb6fb5f406f98a5e9956fe7b7d78a4
tori-calkins <tori.calkins2705@gmail.com>
2023-09-22 23:35:13 -0700

    1.75 of the coding-challenges done and the my answers for interview questions done

--- code-challenges.test.js
+++ code-challenges.test.js
@@ -15,9 +15,9 @@
 // a) Create a test with an expect statement using the variable provided.
 // HINT: Check out this resource: https://jestjs.io/docs/expect#expectarraycontainingarray
 
-// test:
-describe("shuffledColors", () => {
-  it("removes the first item from the array and shuffles the remaining items.", () => {
+// // test:
+// describe("shuffledColors", () => {
+//   it("removes the first item from the array and shuffles the remaining items.", () => {
     const colors1 = ["purple", "blue", "green", "yellow", "pink"]
     // Expected output example (can be a different order): ["yellow", "blue", "pink", "green"]
     const colors2 = [
@@ -29,10 +29,10 @@ describe("shuffledColors", () => {
       "saffron"
     ]
     // Expected output example (can be a different order): ["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]
-    expect(shuffledColors(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
-    expect(shuffledColors(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
-  })
-  })
+  //   expect(shuffledColors(colors1)).toEqual(expect.arrayContaining(["yellow", "blue", "pink", "green"]))
+  //   expect(shuffledColors(colors2)).toEqual(expect.arrayContaining(["saffron", "aquamarine", "periwinkle", "indigo", "ochre"]))
+  // })
+  // })
   // this is my attempt at using the second part of the arrayContaining method and it wouldn't allow my test to pass so I commented it out. Looking at it, it doesn't look like i needed it to begin with but here's what I did when I tried it out. 
   // it("removes the first item from the array and shuffles the remaining items.", () => {
   //   expect(shuffledColors(["purple", "blue", "green", "yellow", "pink"])).not.toEqual(expect.arrayContaining(expected))
@@ -65,26 +65,81 @@ const shuffledColors = (array) => {
 //  PASS  ./code-challenges.test.js
 
 // --------------------2) Create a function that takes in an object that contains up votes and down votes and returns the end tally.
+// use 3rd question of last weeks assessment for help 
+// const accumSum = (array) => {
+//   let sum = 0 
+//   return array.map((value) => (sum += value)) /
+// }
+
 
 // a) Create a test with expect statements for each of the variables provided.
-
-const votes1 = { upVotes: 13, downVotes: 2 }
-// Expected output: 11
-const votes2 = { upVotes: 2, downVotes: 33 }
-// Expected output: -31
-
+// describe("endTally", () => {
+//   it ("returns the end tally", () => {
+    const votes1 = { upVotes: 13, downVotes: 2 }
+    // Expected output: 11
+    const votes2 = { upVotes: 2, downVotes: 33 }
+    // Expected output: -31
+//     expect(endTally(votes1)).toEqual(11)
+//     expect(endTally(votes2)).toEqual(-31)
+//   })
+// })
+//Good Failure: ReferenceError: endTally is not defined
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// function name: endTally
+// input: object
+// output: number of the end tally
+// process: create a function that'll use 
 
 // --------------------3) Create a function that takes in two arrays as arguments and returns one array with no duplicate values. STRETCH: Use the spread operator to pass in a dynamic number of arguments.
 
 // a) Create a test with an expect statement using the variables provided.
-
-const dataTypesArray1 = ["array", "object", "number", "string", "Boolean"]
-const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
-// Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+describe("newArray", () => {
+  it("returns one array with no duplicate values", () => {
+    const dataTypesArray1 = ["array", "object", "number", "string", "Boolean"]
+    const dataTypesArray2 = ["string", "null", "Boolean", "string", "undefined"]
+    // Expected output: ["array", "object", "number", "string", "Boolean", "null", "undefined"]
+    expect(newArray(dataTypesArray1)(dataTypesArray2)).toEqual(["array", "object", "number", "string", "Boolean", "null", "undefined"])
+  })
+})
+// Good failure: ReferenceError: newArray is not defined
 
 // b) Create the function that makes the test pass.
 
 // Pseudo code:
+// function name: newArray
+// input: two arrays
+// output: one new array with the elements of both arrays combined without repetition
+// process: create a function that'll use spread operator to combine the two arrays into one array and then use a nuilt-in method that'll delete all the duplicates inside the new array (...new set()). 
+
+// attempt 1: // looked into the spread operators and though it made more sense than the .concat method so I swithced over to just doing the spread operator function.
+// const newArray = () => {
+//   let arr = dataTypesArray1.concat(dataTypesArray2)
+//   let mergedArr = [...new Set(arr)]
+//     return mergedArr
+// }
+// console.log(dataTypesArray1 + dataTypesArray2) // output: array,object,number,string,Booleanstring,null,Boolean,string,undefined
+
+
+// attempt 2:
+const newArray = (arr1, arr2) => { // function with arr1 and arr2 as parameters
+  const combinedArr = [...arr1, ...arr2] // uses spread operators to combine the two arrays into one array
+  const newArr = [...new Set(combinedArr)] // the new Set() created a new object that took out all the duplicates and the spread operator converted the object back into an array
+  return newArr // returned what was in the newArray function
+}
+const finalResultsArray = newArray(dataTypesArray1, dataTypesArray2) // created a final array to hold what the newArray did and call on the test variables given 
+//console.log(finalResultsArray) // console.log to make sure it all worked
+
+// output:
+// [
+//   'array',
+//   'object',
+//   'number',
+//   'string',
+//   'Boolean',
+//   'null',
+//   'undefined'
+// ]
+
+// worked for console.log but not for Jest test
--- interview-questions.md
+++ interview-questions.md
@@ -6,35 +6,35 @@ First, without external resources. Challenge yourself to answer from memory.
 
 Then, research the question to expand on your answer. Even if you feel you have answered the question completely on your own, there is always something more to learn. Write your researched answer in your OWN WORDS.
 
-Type your initials here to confirm you read the instructions:
+Type your initials here to confirm you read the instructions: tc
 
 1. What is object-oriented programming? How is it different than functional programming?
 
-Your answer:
+Your answer: Object-oriented programming is when the program reads everything as an object and functionality happens through built-in methods. Functional programming uses functions to make actions happen within a code. 
 
 Researched answer:
 
 2. Ruby has an implicit return. What does that mean?
 
-Your answer:
+Your answer: An implicit return is a return that the computer knows and automatically happens behind the scenes without having to be typed or placed within the code.
 
 Researched answer:
 
 3. What is a block in Ruby?
 
-Your answer:
+Your answer: A block in Ruby is a encapsulated chunk of code that can either be passed to an object or to a Ruby method. A block is encapsulated by do and end or curly braces {}.
 
 Researched answer:
 
 4. How do you extract a single value from a Ruby hash?
 
-Your answer:
+Your answer: To extract a single value from a Ruby hash, the developer would use square brackets with a colon inside followed by the key of the key:value pair to extract the value from the hash.
 
 Researched answer:
 
 5. STRETCH: What is a version manager?
 
-Your answer:
+Your answer: I am not fully sure, but I do know a manager is in control so I'd like to say a version manager, manages the version of code being used in a specific coding language. 
 
 Researched answer:
 
